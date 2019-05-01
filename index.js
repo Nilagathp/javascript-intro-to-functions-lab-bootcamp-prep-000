34e8ec7315ba1956fdc983dd9a49469a4ee23c91
empathetic-parameter-3158 <nilagathp@gmail.com>
2019-05-01 05:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(strig) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+    return "YES INDEED!";
   }
   if (string === "I love you, Grandma") {
-    return "I love you, too."
+    return "I love you, too.";
   }
 }
